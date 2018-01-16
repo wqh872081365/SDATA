b709822801b9d0e2f97a940fe2d2199431453ca1
wangqihui <wangqihui0324@gmail.com>
2018-01-16 12:00:03 +0800

    add fabfile

---
+++ fabfile-mac.py
@@ -0,0 +1,32 @@
+from fabric.api import *
+
+code_path = "/Users/wangqihui/Documents/wangqihui/SDATA"
+env_path = "/Users/wangqihui/Documents/wangqihui/python/python3_env/sdata"
+
+
+def start_scrapyd():
+    local(env_path + "/bin/scrapyd")
+
+
+def pip_upgrade():
+    local(env_path + "/bin/pip3.6 install -r requirements.txt")
+
+
+def list_scrapyd_all():
+    local(env_path + "/bin/scrapyd-deploy -l")
+
+
+def scrapyd_deploy():
+    local(env_path + "/bin/scrapyd-deploy")
+
+
+def show_scrapyd_status():
+    local(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py")
+
+
+def delete_scrapyd_project():
+    local(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py" + " del_project")
+
+
+def add_scrapyd_schedule(spider):
+    local(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py" + " add_schedule " + spider)
