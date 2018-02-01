289a0bf59aa1d21c33510f6872b6007f325e2609
wangqihui <wangqihui0324@gmail.com>
2018-02-01 19:43:41 +0800

    update fabfile

--- fabfile-dev.py
+++ fabfile-dev.py
@@ -1,75 +1,143 @@
 from fabric.api import *
 
 code_path = "/home/wqh/wangqihui/work/SDATA"
+env_root = "/home/wqh/wangqihui/work/env/python3_env"
 env_path = "/home/wqh/wangqihui/work/env/python3_env/sdata"
+git_repository = "git@github.com:wqh872081365/SDATA.git"
 
 
-def start_scrapyd():
-    local(env_path + "/bin/scrapyd")
+# system
+def reload_nginx():
+    local("sudo nginx -s reload")
 
+
+# sdata
+def pip_upgrade():
+    local(env_path + "/bin/pip3.6 install -r " + code_path + "/requirements.txt")
+
+def migrate_sdata():
+    local(env_path + "/bin/python3.6 " + code_path + "/manage.py " + "migrate")
+
+def update_code(branch="master"):
+    local("git pull origin %s" % (branch,))
+
+def update_static():
+    local(env_path + "/bin/python3.6 " + code_path + "/manage.py " + "collectstatic --noinput")
+
+def start_sdata():
+    local("sudo supervisorctl start sdata_gunicorn")
+    local("sudo rm -f " + code_path + "/maintenance.html")
+
+def stop_sdata():
+    local("sudo supervisorctl stop sdata_gunicorn")
+    local("sudo cp -f " + code_path + "/templates/maintenance.html " + code_path + "/maintenance.html")
+
+def update_user_logs(user_log_id):
+    local(env_path + "/bin/python3.6 " + code_path + "/manage.py " + "update_user_logs " + user_log_id)
+
+
+# rq
+def start_rq_scheduler():
+    local("sudo supervisorctl start sdata_rqscheduler")
 
 def start_rqworker():
-    local(env_path + "/bin/python3.6 " + code_path + "/manage.py " + "rqworker default high low add_spider spider_pipeline scheduler spider_status rq_worker_number_control proxy_valid")
+    local("sudo supervisorctl start sdata_rq_default sdata_rq_high sdata_rq_low sdata_rq_add_spider sdata_rq_spider_pipeline sdata_rq_scheduler sdata_rq_spider_status sdata_rq_worker_number_control sdata_rq_proxy_valid")
 
-
-def start_rq_scheduler():
-    local(env_path + "/bin/python3.6 " + code_path + "/manage.py " + "rqscheduler")
-
-
-def start_pgadmin4():
-    local("/home/wqh/wangqihui/work/env/python3_env/pgadmin4/bin/python3.6 " + "/home/wqh/wangqihui/work/env/python3_env/pgadmin4/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py")
-
-
-def pip_upgrade():
-    local(env_path + "/bin/pip3.6 install -r requirements.txt")
-
-
-def list_scrapyd_all():
-    local(env_path + "/bin/scrapyd-deploy -l")
-
-
-def scrapyd_deploy():
-    local(env_path + "/bin/scrapyd-deploy")
-
-
-def show_scrapyd_status():
-    local(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py")
-
-
-def delete_scrapyd_project():
-    local(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py" + " del_project")
-
-
-def add_scrapyd_schedule(spider):
-    local(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py" + " add_schedule " + spider)
-
+def restart_rqworker():
+    local("sudo supervisorctl restart sdata_rq_default sdata_rq_high sdata_rq_low sdata_rq_add_spider sdata_rq_spider_pipeline sdata_rq_scheduler sdata_rq_spider_status sdata_rq_worker_number_control sdata_rq_proxy_valid")
 
 def add_rq_job():
     local(env_path + "/bin/python3.6 " + code_path + "/manage.py " + "rq_job")
 
 
-def start_supervisor():
-    local("supervisorctl start sdata_rq_default")
+# scrapy
+def start_scrapyd():
+    local("sudo supervisorctl start sdata_scrapyd")
 
+def list_scrapyd_all():
+    with lcd(code_path + "/wdata"):
+        local(env_path + "/bin/scrapyd-deploy -l")
+
+def scrapyd_deploy():
+    with lcd(code_path + "/wdata"):
+        local(env_path + "/bin/scrapyd-deploy")
+
+def show_scrapyd_status():
+    local(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py")
+
+def delete_scrapyd_project():
+    local(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py" + " del_project")
+
+def add_scrapyd_schedule(spider):
+    local(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py" + " add_schedule " + spider)
+
+
+# postgres
+def start_pgadmin4():
+    local(env_root + "/pgadmin4/bin/python3.6 " + env_root + "/pgadmin4/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py")
 
 def clean_postgres():
     pass
 
 
-def prepare_os():
+# log
+def clean_log():
     pass
 
 
+# update file
+def update_supervisor():
+    local("sudo cp -f " + code_path + "/deploy/sdata_supervisor_dev.conf " + "/etc/supervisor/conf.d/sdata_supervisor_dev.conf")
+    local("sudo supervisorctl reload")
+
+def update_settings():
+    # local("sudo cp -f " + code_path + "/deploy/local_settings_aliyun.py " + code_path + "/SDATA/local_settings.py")
+    local("sudo supervisorctl restart sdata_gunicorn")
+
+def update_nginx():
+    local("sudo cp -f " + code_path + "/deploy/sdata_nginx.conf " + "/etc/nginx/sites-available/sdata_nginx.conf")
+    local("sudo ln -sf /etc/nginx/sites-available/sdata_nginx.conf /etc/nginx/sites-enabled/sdata_nginx.conf")
+    execute(reload_nginx)
+
+
+# other
+def prepare_file():
+    with lcd("/home"):
+        local("mkdir -p wangqihui.me")
+        with lcd("wanqihui.me"):
+            local("mkdir -p wanqihui")
+            with lcd("wanqihui"):
+                local("git clone " + git_repository)
+                local("mkdir -p SDATA_LOG")
+                with lcd("SDATA_LOG"):
+                    local("mkdir -p gunicorn")
+                    local("mkdir -p rq")
+                    local("mkdir -p scrapy")
+                local("mkdir -p python")
+                with lcd("python"):
+                    local("mkdir -p python3_env")
+                    with lcd("python3_env"):
+                        local("pyenv shell 3.6.3")
+                        local("virtualenv SDATA")
+                        local("pyenv shell --unset")
+
+
+def prepare_os():
+    local("sudo apt-get update")
+    local("sudo apt-get upgrade")
+
+
 def deploy():
+    execute(stop_sdata)
+    execute(update_code)
     execute(pip_upgrade)
+    execute(migrate_sdata)
+    execute(update_static)
+    execute(start_sdata)
+    execute(reload_nginx)
 
-    # execute(start_scrapyd)
-
-    # execute(start_rqworker)
-    # execute(start_rq_scheduler)
-
+    execute(start_rq_scheduler)
+    execute(start_rqworker)
     execute(add_rq_job)
 
-
-def update_user_logs(user_log_id):
-    local(env_path + "/bin/python3.6 " + code_path + "/manage.py " + "update_user_logs " + user_log_id)
+    execute(start_scrapyd)
--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,85 @@
 from fabric.api import *
 
-code_path = ""
-env_path = ""
+code_path = "/home/wangqihui.me/wangqihui/SDATA"
+env_root = "/home/wangqihui.me/wangqihui/python/python3_env"
+env_path = "/home/wangqihui.me/wangqihui/python/python3_env/SDATA"
+git_repository = "git@github.com:wqh872081365/SDATA.git"
 
-env.roledefs['apps'] = [
+env.roledefs["apps"] = []
+env.roledefs["db"] = []
+env.roledefs["static"] = []
+env.roledefs["job"] = []
 
-]
 
-
+# system
 @parallel
 @roles('apps')
-def start_scrapyd():
-    with cd(code_path + "/wdata"):
-        run(env_path + "/bin/scrapyd")
+def reload_nginx():
+    sudo("nginx -s reload")
 
 
+# sdata
 @parallel
 @roles('apps')
 def pip_upgrade():
-    with cd(code_path):
-        run(env_path + "/bin/pip3.6 install -r requirements.txt")
+    run(env_path + "/bin/pip3.6 install -r " + code_path + "/requirements.txt")
 
+@roles('db')
+def migrate_sdata():
+    run(env_path + "/bin/python3.6 " + code_path + "/manage.py " + "migrate")
+
+@parallel
+@roles('apps')
+def update_code(branch="master"):
+    run("git pull origin %s" % (branch,))
+
+@roles('static')
+def update_static():
+    run(env_path + "/bin/python3.6 " + code_path + "/manage.py " + "collectstatic --noinput")
+
+@parallel
+@roles('apps')
+def start_sdata():
+    sudo("supervisorctl start sdata_gunicorn")
+    sudo("rm -f " + code_path + "/maintenance.html")
+
+@parallel
+@roles('apps')
+def stop_sdata():
+    sudo("supervisorctl stop sdata_gunicorn")
+    sudo("cp -f " + code_path + "/templates/maintenance.html " + code_path + "/maintenance.html")
+
+@roles('job')
+def update_user_logs(user_log_id):
+    run(env_path + "/bin/python3.6 " + code_path + "/manage.py " + "update_user_logs " + user_log_id)
+
+
+# rq
+@parallel
+@roles('apps')
+def start_rq_scheduler():
+    sudo("supervisorctl start sdata_rqscheduler")
+
+@parallel
+@roles('apps')
+def start_rqworker():
+    sudo("supervisorctl start sdata_rq_default sdata_rq_high sdata_rq_low sdata_rq_add_spider sdata_rq_spider_pipeline sdata_rq_scheduler sdata_rq_spider_status sdata_rq_worker_number_control sdata_rq_proxy_valid")
+
+@parallel
+@roles('apps')
+def restart_rqworker():
+    sudo("supervisorctl restart sdata_rq_default sdata_rq_high sdata_rq_low sdata_rq_add_spider sdata_rq_spider_pipeline sdata_rq_scheduler sdata_rq_spider_status sdata_rq_worker_number_control sdata_rq_proxy_valid")
+
+@roles('job')
+def add_rq_job():
+    run(env_path + "/bin/python3.6 " + code_path + "/manage.py " + "rq_job")
+
+
+# scrapy
+@parallel
+@roles('apps')
+def start_scrapyd():
+    sudo("supervisorctl start sdata_scrapyd")
 
 @parallel
 @roles('apps')
@@ -28,27 +87,109 @@ def list_scrapyd_all():
     with cd(code_path + "/wdata"):
         run(env_path + "/bin/scrapyd-deploy -l")
 
-
 @parallel
 @roles('apps')
 def scrapyd_deploy():
     with cd(code_path + "/wdata"):
         run(env_path + "/bin/scrapyd-deploy")
 
-
 @parallel
 @roles('apps')
 def show_scrapyd_status():
     run(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py")
-
 
 @parallel
 @roles('apps')
 def delete_scrapyd_project():
     run(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py" + " del_project")
 
+@roles('job')
+def add_scrapyd_schedule(spider):
+    run(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py" + " add_schedule " + spider)
+
+
+# postgres
+@roles('db')
+def start_pgadmin4():
+    run(env_root + "/pgadmin4/bin/python3.6 " + env_root + "/pgadmin4/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py")
+
+@roles('db')
+def clean_postgres():
+    pass
+
+
+# log
+@parallel
+@roles('apps')
+def clean_log():
+    pass
+
+
+# update file
+@parallel
+@roles('apps')
+def update_supervisor():
+    sudo("cp -f " + code_path + "/deploy/sdata_supervisor_dev.conf " + "/etc/supervisor/conf.d/sdata_supervisor_dev.conf")
+    sudo("supervisorctl reload")
 
 @parallel
 @roles('apps')
-def add_scrapyd_schedule(spider):
-    run(env_path + "/bin/python3.6 " + code_path + "/app/utils/scrapy.py" + " add_schedule " + spider)
+def update_settings():
+    sudo("scp -f " + code_path + "/deploy/local_settings_aliyun.py " + " %s@%s:" % ((env.user, env.host)) + code_path + "/SDATA/local_settings.py")
+    sudo("supervisorctl restart sdata_gunicorn")
+
+@parallel
+@roles('apps')
+def update_nginx():
+    sudo("cp -f " + code_path + "/deploy/sdata_nginx.conf " + "/etc/nginx/sites-available/sdata_nginx.conf")
+    sudo("ln -sf /etc/nginx/sites-available/sdata_nginx.conf /etc/nginx/sites-enabled/sdata_nginx.conf")
+    execute(reload_nginx)
+
+
+# other
+@parallel
+@roles('apps')
+def prepare_file():
+    with cd("/home"):
+        run("mkdir -p wangqihui.me")
+        with cd("wanqihui.me"):
+            run("mkdir -p wanqihui")
+            with cd("wanqihui"):
+                run("git clone " + git_repository)
+                run("mkdir -p SDATA_LOG")
+                with cd("SDATA_LOG"):
+                    run("mkdir -p gunicorn")
+                    run("mkdir -p rq")
+                    run("mkdir -p scrapy")
+                    run("mkdir -p python")
+                with cd("python"):
+                    run("mkdir -p python3_env")
+                    with cd("python3_env"):
+                        run("pyenv shell 3.6.3")
+                        run("virtualenv SDATA")
+                        run("pyenv shell --unset")
+
+
+@parallel
+@roles('apps')
+def prepare_os():
+    sudo("apt-get update")
+    sudo("apt-get upgrade")
+
+
+@parallel
+@roles('apps')
+def deploy():
+    execute(stop_sdata)
+    execute(update_code)
+    execute(pip_upgrade)
+    execute(migrate_sdata)
+    execute(update_static)
+    execute(start_sdata)
+    execute(reload_nginx)
+
+    execute(start_rq_scheduler)
+    execute(start_rqworker)
+    execute(add_rq_job)
+
+    execute(start_scrapyd)
