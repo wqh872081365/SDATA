c316f8c6fbc54fa3ce09f657f52c87b82f56bfb1
wangqihui <wangqihui0324@gmail.com>
2018-02-06 19:34:50 +0800

    fix nginx

--- SDATA/settings.py
+++ SDATA/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
