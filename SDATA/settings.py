788f44b272410856b54cadb1e14925123439fabd
wangqihui <wangqihui0324@gmail.com>
2018-02-06 19:54:18 +0800

    fix static

--- SDATA/settings.py
+++ SDATA/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'kaola/static'),
+    os.path.join(BASE_DIR, 'SDATA/static'),
 )
 
 STATICFILES_FINDERS = (
