46db3041a829de2b293acd29dcbda3f6a5ba53b8
wangqihui <wangqihui0324@gmail.com>
2018-02-06 19:51:34 +0800

    fix static

--- SDATA/settings.py
+++ SDATA/settings.py
@@ -125,6 +125,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'kaola/static'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
