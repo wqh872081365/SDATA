c6cd077ce696e1645118d536777840f202ecd75e
wangqihui <wangqihui0324@gmail.com>
2018-02-08 17:06:09 +0800

    travis test1

--- SDATA/settings.py
+++ SDATA/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'SDATA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sdata',
+        'USER': 'sdata',
+        'PASSWORD': 'sdata',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
