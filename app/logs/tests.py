4a7e939ad57b134bc581152d34edee900a0bbb02
wangqihui <wangqihui0324@gmail.com>
2018-02-08 16:28:21 +0800

    test travis

--- app/logs/tests.py
+++ app/logs/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from app.logs.models import UserLog
-from fabfile_dev import start_scrapyd
+from app.logs.add_log import add_spider_log
+from scrapy.http import Response
 # Create your tests here.
 
 
@@ -9,6 +9,8 @@ class UserLogTestCase(TestCase):
     def setUp(self):
         pass
 
-
-    def test_find_user_log_in_season(self):
-        pass
+    def test_add_spider_log(self):
+        log = add_spider_log(user_log_id=1, source="0", source_id=1, url="test_url", status="2", msg="test_msg", response=Response(url="test_url", body="test_body".encode("utf-8")))
+        self.assertEqual(log.logs.get("fg_msg")[0].get("msg", "") if log.logs and isinstance(log.logs, (dict,)) and log.logs.get("fg_msg", []) and isinstance(log.logs.get("fg_msg", []), (list, tuple)) and log.logs.get("fg_msg")[0] and isinstance(log.logs.get("fg_msg")[0], dict) else "", "test_msg")
+        self.assertEqual(log.logs.get("fg_msg")[0].get("url", "") if log.logs and isinstance(log.logs, (dict,)) and log.logs.get("fg_msg", []) and isinstance(log.logs.get("fg_msg", []), (list, tuple)) and log.logs.get("fg_msg")[0] and isinstance(log.logs.get("fg_msg")[0], dict) else "", "test_url")
+        self.assertEqual(log.logs.get("fg_msg")[0].get("data", "") if log.logs and isinstance(log.logs, (dict,)) and log.logs.get("fg_msg", []) and isinstance(log.logs.get("fg_msg", []), (list, tuple)) and log.logs.get("fg_msg")[0] and isinstance(log.logs.get("fg_msg")[0], dict) else "", "test_body")
